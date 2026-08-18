fd6320876fa08ff782cd39e50fb6f6f295acd85e
Smudge <JXS1787@student.bham.ac.uk>
2026-08-18 19:42:45 +0100

    update

--- src/main/java/com/github/smuddgge/leaf/datatype/User.java
+++ src/main/java/com/github/smuddgge/leaf/datatype/User.java
@@ -262,7 +262,7 @@ public class User {
         String[] parts = message.split("::");
 
         boolean hasTitle = false;
-        Title.Times times = Title.Times.of(
+        Title.Times times = Title.Times.times(
                 Duration.ofMillis(1000),
                 Duration.ofMillis(1000),
                 Duration.ofMillis(1000)
@@ -342,7 +342,7 @@ public class User {
                 ? Integer.parseInt(durations[3]) : 1000;
 
         // Create durations.
-        return Title.Times.of(
+        return Title.Times.times(
                 Duration.ofMillis(fadeIn),
                 Duration.ofMillis(stay),
                 Duration.ofMillis(fadeOut)
